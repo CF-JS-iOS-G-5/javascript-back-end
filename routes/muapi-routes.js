1da1627ce0226367f1339001acffa72935cda664
Patrick Sheridan <sheridp2@gmail.com>
2017-05-17 18:14:29 -0700

    hotfix for heroku

--- routes/muapi-routes.js
+++ routes/muapi-routes.js
@@ -7,10 +7,10 @@ const rp = require('request-promise');
 
 module.exports = function(router) {
 
-  router.get('/meetup/events', (req,res) => {
+  router.get('/meetup/events/:language', (req,res) => {
     debug('#get ./api/meetup/events');
     console.log(req.body);
-    let route = `https://api.meetup.com/find/events?sign=true&photo-host=public&text=${req.body.language}&key=${process.env.API_KEY}&radius=5&omit=created,updated,venue.id,venue.repinned,venue.country,venue.localized_country_name,venue.phone,group.created,group.lat,group.lon,group.who`;
+    let route = `https://api.meetup.com/find/events?sign=true&photo-host=public&text=${req.params.language}&key=${process.env.API_KEY}&radius=5&omit=created,updated,venue.id,venue.repinned,venue.country,venue.localized_country_name,venue.phone,group.created,group.lat,group.lon,group.who`;
 
     rp(route)
     .then(data => {
