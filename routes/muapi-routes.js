d893f0cfc1abe4000ad7a3fdae63dc5c7a806cee
Patrick Sheridan <sheridp2@gmail.com>
2017-05-17 17:55:37 -0700

    meet up api route working

--- routes/muapi-routes.js
+++ routes/muapi-routes.js
@@ -3,11 +3,36 @@
 const debug = require('debug')('bizapp:mu-routes');
 const createError = require('http-errors');
 const ctrlMu = require('../controllers/mu-controller');
+const rp = require('request-promise');
 
 module.exports = function(router) {
 
-  router.get('./api/meetup/events', (req, res) => {
+  router.get('/meetup/events', (req,res) => {
     debug('#get ./api/meetup/events');
-    getMuRoute(req);
+    console.log(req.body);
+    let route = `https://api.meetup.com/find/events?sign=true&photo-host=public&text=${req.body.language}&key=${process.env.API_KEY}&radius=5&omit=created,updated,venue.id,venue.repinned,venue.country,venue.localized_country_name,venue.phone,group.created,group.lat,group.lon,group.who`;
+
+    rp(route)
+    .then(data => {
+      console.log(data);
+      return JSON.parse(data);
+    })
+    .then(body => {
+      console.log(body);
+      res.json(body);
+    })
+    .catch(err => res.status(err.status).send(err.message));
+  //   rp(route, function(data) {
+  //     console.log(data);
+  //     return data;
+  //   }
+  //   .then(events => events)
+  //   .catch(err => createError(400, err.message))
+  // )
+  // .then(events => events)
+  // .catch(err => createError(400, err.message));
+  // });
+  // return router;
   });
+  return router;
 };
